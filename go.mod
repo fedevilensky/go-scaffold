29e11483ab19c7c3367eb13914ac3f819b3a9ee6
Federico Vilensky <fedevilensky@gmail.com>
2022-08-03 18:24:35 -0300

    updated dependencies

--- go.mod
+++ go.mod
@@ -3,22 +3,23 @@ module github.com/fedevilensky/go-scaffold
 go 1.18
 
 require (
-	github.com/charmbracelet/bubbles v0.10.3
-	github.com/charmbracelet/bubbletea v0.20.0
+	github.com/charmbracelet/bubbles v0.13.0
+	github.com/charmbracelet/bubbletea v0.22.0
 	github.com/charmbracelet/lipgloss v0.5.0
-	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739
+	github.com/muesli/termenv v0.12.0
 )
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/charmbracelet/harmonica v0.1.0 // indirect
+	github.com/charmbracelet/harmonica v0.2.0 // indirect
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	golang.org/x/sys v0.0.0-20220315194320-039c03cc5b86 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	github.com/rivo/uniseg v0.3.1 // indirect
+	golang.org/x/sys v0.0.0-20220803195053-6e608f9ce704 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
 )
--- vendor/github.com/muesli/cancelreader/cancelreader_linux.go
+++ vendor/github.com/muesli/cancelreader/cancelreader_linux.go
@@ -1,8 +1,7 @@
 //go:build linux
 // +build linux
 
-// nolint:revive
-package tea
+package cancelreader
 
 import (
 	"errors"
@@ -14,14 +13,14 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// newCancelReader returns a reader and a cancel function. If the input reader
-// is an *os.File, the cancel function can be used to interrupt a blocking call
-// read call. In this case, the cancel function returns true if the call was
-// cancelled successfully. If the input reader is not a *os.File, the cancel
-// function does nothing and always returns false. The linux implementation is
-// based on the epoll mechanism.
-func newCancelReader(reader io.Reader) (cancelReader, error) {
-	file, ok := reader.(*os.File)
+// NewReader returns a reader and a cancel function. If the input reader is a
+// File, the cancel function can be used to interrupt a blocking read call.
+// In this case, the cancel function returns true if the call was canceled
+// successfully. If the input reader is not a File, the cancel function
+// does nothing and always returns false. The Linux implementation is based on
+// the epoll mechanism.
+func NewReader(reader io.Reader) (CancelReader, error) {
+	file, ok := reader.(File)
 	if !ok {
 		return newFallbackCancelReader(reader)
 	}
@@ -38,6 +37,7 @@ func newCancelReader(reader io.Reader) (cancelReader, error) {
 
 	r.cancelSignalReader, r.cancelSignalWriter, err = os.Pipe()
 	if err != nil {
+		_ = unix.Close(epoll)
 		return nil, err
 	}
 
@@ -46,7 +46,8 @@ func newCancelReader(reader io.Reader) (cancelReader, error) {
 		Fd:     int32(file.Fd()),
 	})
 	if err != nil {
-		return nil, fmt.Errorf("add reader to epoll interrest list")
+		_ = unix.Close(epoll)
+		return nil, fmt.Errorf("add reader to epoll interest list")
 	}
 
 	err = unix.EpollCtl(epoll, unix.EPOLL_CTL_ADD, int(r.cancelSignalReader.Fd()), &unix.EpollEvent{
@@ -54,28 +55,29 @@ func newCancelReader(reader io.Reader) (cancelReader, error) {
 		Fd:     int32(r.cancelSignalReader.Fd()),
 	})
 	if err != nil {
-		return nil, fmt.Errorf("add reader to epoll interrest list")
+		_ = unix.Close(epoll)
+		return nil, fmt.Errorf("add reader to epoll interest list")
 	}
 
 	return r, nil
 }
 
 type epollCancelReader struct {
-	file               *os.File
-	cancelSignalReader *os.File
-	cancelSignalWriter *os.File
+	file               File
+	cancelSignalReader File
+	cancelSignalWriter File
 	cancelMixin
 	epoll int
 }
 
 func (r *epollCancelReader) Read(data []byte) (int, error) {
-	if r.isCancelled() {
-		return 0, errCanceled
+	if r.isCanceled() {
+		return 0, ErrCanceled
 	}
 
 	err := r.wait()
 	if err != nil {
-		if errors.Is(err, errCanceled) {
+		if errors.Is(err, ErrCanceled) {
 			// remove signal from pipe
 			var b [1]byte
 			_, readErr := r.cancelSignalReader.Read(b[:])
@@ -91,7 +93,7 @@ func (r *epollCancelReader) Read(data []byte) (int, error) {
 }
 
 func (r *epollCancelReader) Cancel() bool {
-	r.setCancelled()
+	r.setCanceled()
 
 	// send cancel signal
 	_, err := r.cancelSignalWriter.Write([]byte{'c'})
@@ -145,7 +147,7 @@ func (r *epollCancelReader) wait() error {
 	case int32(r.file.Fd()):
 		return nil
 	case int32(r.cancelSignalReader.Fd()):
-		return errCanceled
+		return ErrCanceled
 	}
 
 	return fmt.Errorf("unknown error")
